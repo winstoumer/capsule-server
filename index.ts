6846d5e773b32b3aad536237550869263c1accbb
winstoumer <winstoumer@proton.me>
2024-05-12 19:35:50 +0200

    modified:   index.ts 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import * as dotenv from 'dotenv';
+import * as cors from 'cors';
 
 dotenv.config();
 
@@ -12,16 +13,13 @@ import { currentMiningRouter } from './api/currentMining';
 
 const app = express();
 
+app.use(cors());
+
 app.use('/api/balance', balanceRouter);
-
 app.use('/api/referral', referralRouter);
-
 app.use('/api/user', userRouter);
-
 app.use('/api/task', taskRouter);
-
 app.use('/api/matter', matterRouter);
-
 app.use('/api/currentMining', currentMiningRouter);
 
 const PORT = process.env.PORT || 3002;
