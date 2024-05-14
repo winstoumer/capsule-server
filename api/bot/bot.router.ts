9c43fe5828280a23681d656dafdbe86f1ba1ed75
winstoumer <winstoumer@proton.me>
2024-05-14 22:43:30 +0200

    modified:   .env 	modified:   api/bot/bot.router.ts

--- api/bot/bot.router.ts
+++ api/bot/bot.router.ts
@@ -5,11 +5,13 @@ const TelegramBot = require('node-telegram-bot-api');
 
 const botRouter = Router();
 
+const token = '7129507448:AAE3uaePuXH_9SmV5KM6TnYbuzJuSsEoB3M';
+
 botRouter.post('/sendReferralMessage', async (req: any, res: any) => {
     const { telegramUserId } = req.body;
 
     try {
-        const bot = new TelegramBot(process.env.TELEGRAM_BOT_TOKEN, { polling: true });
+        const bot = new TelegramBot(token, { polling: true });
         await bot.sendMessage(telegramUserId, `Ваш реферальный код:  https://t.me/gbaswebtest_bot?start=${telegramUserId}`);
         res.sendStatus(200);
     } catch (error) {
