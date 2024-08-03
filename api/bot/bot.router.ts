991ac5fa1447b196672374903ba9ae29725a6ea3
winstoumer <126259578+winstoumer@users.noreply.github.com>
2024-08-03 16:25:50 +0200

    modified:   api/bot/bot.router.ts 	modified:   index.ts

--- api/bot/bot.router.ts
+++ api/bot/bot.router.ts
@@ -27,7 +27,7 @@ app.post(`/webhook/${process.env.WEBHOOK_SECRET_PATH}`, (req, res) => {
 const channelId = '-1002165541344';
 
 // Условие для отправки сообщения при запуске
-const sendMessageOnStart = true;
+const sendMessageOnStart = false;
 
 // Функция для отправки и закрепления сообщения в канале
 const sendMessageToChannel = async () => {
@@ -62,7 +62,8 @@ const portalIntervals = [
     { open: { hour: 6, minute: 10 }, close: { hour: 6, minute: 40 } },
     { open: { hour: 9, minute: 10 }, close: { hour: 9, minute: 40 } },
     { open: { hour: 12, minute: 10 }, close: { hour: 12, minute: 40 } },
-    { open: { hour: 16, minute: 10 }, close: { hour: 16, minute: 40 } },
+    { open: { hour: 12, minute: 50 }, close: { hour: 16, minute: 30 } },
+    { open: { hour: 16, minute: 10 }, close: { hour: 17, minute: 40 } },
     { open: { hour: 18, minute: 10 }, close: { hour: 18, minute: 40 } },
     { open: { hour: 19, minute: 10 }, close: { hour: 19, minute: 40 } },
     { open: { hour: 21, minute: 30 }, close: { hour: 22, minute: 10 } },
